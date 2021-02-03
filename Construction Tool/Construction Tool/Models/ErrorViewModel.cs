d38db364ae77dbea5f2fbe707bddc63da072cee8
Alec Lund <43127299+AlecLund@users.noreply.github.com>
2021-02-03 17:39:41 -0500

    Added Main Project
    Added the main project setup of the web application

---
+++ Construction Tool/Construction Tool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Construction_Tool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
