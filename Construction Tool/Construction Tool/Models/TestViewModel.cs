db184da114ab719453ff468683e5a847ab6bab3f
Alec Lund <43127299+AlecLund@users.noreply.github.com>
2021-04-05 18:16:37 -0400

    version

---
+++ Construction Tool/Construction Tool/Models/TestViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Construction_Tool.Models
+{
+    public class TestViewModel
+    {
+        public string sql{ get; set; }
+    }
+}
